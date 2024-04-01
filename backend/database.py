cda1f1a408a7a1c7bf826390914ee941951f4926
dkutugin3 <143739823+dkutugin3@users.noreply.github.com>
2024-04-01 11:55:28 +0300

    create db and connect alembic

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,16 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./sql_app.db"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
+
+
+# Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
---
+++ backend/users/models.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from database import Base
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(Base):
+    __tablename__ = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
