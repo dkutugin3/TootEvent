8c4d128cfc80af223ac826ce191b73291d231486
dkutugin3 <143739823+dkutugin3@users.noreply.github.com>
2024-04-07 01:47:27 +0300

    add compose file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.auth.router import router as auth_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
+
 app.include_router(auth_router)
 
--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -2,11 +2,10 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+from app.config import settings
+from app.users.models import Users
 from app.database import Base
-from app.users.models import User
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -16,6 +15,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL_psycopg)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
