73c1c83c7bed99f20dffa9e1762927de4e67dd57
dkutugin3 <143739823+dkutugin3@users.noreply.github.com>
2024-05-20 11:57:34 +0300

    repair CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import all_routers
 
 app = FastAPI(title="TootEvent")
 for router in all_routers:
     app.include_router(router)
 if __name__ == "__main__":
+    origins = [
+    "*",
+]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     uvicorn.run(app="main:app", reload=True, host="0.0.0.0")
